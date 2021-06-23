53b7c3642c325256daf7b6d36f3da023b1ef9bfc
Mohammad Inshal <58660138+Inshal07@users.noreply.github.com>
2021-06-23 09:06:15 +0530

    Update environment.ts
    keys removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase :{
-    apiKey: "AIzaSyBy0oBbQWD87P789NK5xD9rUkrFbtZ7SOw",
-    authDomain: "dashboard-user-34fbc.firebaseapp.com",
-    projectId: "dashboard-user-34fbc",
-    storageBucket: "dashboard-user-34fbc.appspot.com",
-    messagingSenderId: "303256378469",
-    appId: "1:303256378469:web:ea578c2fd5a289e5a73a8c",
-    measurementId: "G-QFKDPD4NTJ"
+    apiKey: "xyz",
+    authDomain: "xyz",
+    projectId: "xyz",
+    storageBucket: "xyz",
+    messagingSenderId: "xyz",
+    appId: "xyz",
+    measurementId: "xyz"
   },
 };
 
